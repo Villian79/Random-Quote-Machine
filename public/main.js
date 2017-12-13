cde50533c23d45a8ec2d60c55abfb70284ccebcd
Igor Vilinchuk <ivilinchuk@gmail.com>
2017-12-13 15:55:09 +0800

    Add: twitter button. Have to reconsider its usage. Add flex-box model for index.html

--- public/main.js
+++ public/main.js
@@ -23,4 +23,21 @@ var requestHandler = function(data){
     $('#author').text(data[0].title);
 }
 
-var errorHandler = err => console.log(`There was a problem while executing your JSONP request...`);
+var errorHandler = err => console.log("There was a problem with your request to the server...");
+
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+  return t;
+}(document, "script", "twitter-wjs"));
